56eb6e9181ecb3633297e90e8ea9010ade6c06e1
Reuben John <rjjohn1@ualberta.ca>
2024-09-26 18:36:15 -0600

    ReubenJohn extends Shape with Circle

--- app/src/main/java/Shape.java
+++ app/src/main/java/Shape.java
@@ -2,8 +2,8 @@ class Shape {
     private int x;
     private int y;
 
-    public Shape(int xval, int yval) {
-        this.x = xval;
-        this.y = yval;
+    public Shape(int x_val, int y_val) {
+        this.x = x_val;
+        this.y = y_val;
     }
 }

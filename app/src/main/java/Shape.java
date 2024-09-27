b5a06c1804a2b0f2556b7ecf89c4d7ea572bc972
Newvber <weibo2@ualberta.ca>
2024-09-26 18:56:01 -0600

    conflict

--- app/src/main/java/Shape.java
+++ app/src/main/java/Shape.java
@@ -1,7 +1,7 @@
 public abstract class Shape {
     private int x;
     private int y;
-    private String color = "red";
+    private String color = "violet";
 
     public Shape(int xval, int yval) {
         this.x = xval;

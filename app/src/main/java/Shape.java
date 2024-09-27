60e0e127c2344004143c98bba2cfa1be661c4c96
Pei Zhang <peilun.zhn@gmail.com>
2024-09-26 18:31:25 -0600

    PeiZhang 2nd Commit

--- app/src/main/java/Shape.java
+++ app/src/main/java/Shape.java
@@ -1,7 +1,8 @@
 class Shape {
     private int x;
     private int y;
-
+    private String color = "blue";
+	    
     public Shape(int xval, int yval) {
         this.x = xval;
         this.y = yval;

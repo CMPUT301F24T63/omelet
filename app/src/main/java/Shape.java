a87608cdd8558a7089d9af32368183780deaf674
Longfei Jiao <ljiao@ualberta.ca>
2024-09-27 13:39:07 -0600

    resolved conflict

--- app/src/main/java/Shape.java
+++ app/src/main/java/Shape.java
@@ -1,11 +1,7 @@
 public abstract class Shape {
     private int x;
     private int y;
-<<<<<<< HEAD
-    private String color = "red";
-=======
     private String color = "white";
->>>>>>> 84ea94d4661e0ae0459e92d0dbe32a89cfc90c15
 
     // this is on test branch
 

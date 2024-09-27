c4721b7e7342d0abd01de12b55913481d3f5a12a
“Dingjingmu <stevenydjm@gmail.com>
2024-09-26 18:23:16 -0600

    d

---
+++ app/src/main/java/big_circle.java
@@ -0,0 +1,5 @@
+class big_circle{
+    public circle extend Shape(int x,int y){
+        super(x,y);
+    }
+}
--- app/src/main/java/circle.java
+++
@@ -1,5 +0,0 @@
-class circle{
-    public circle extend shape(int x,int y){
-        super(x,y);
-    }
-}

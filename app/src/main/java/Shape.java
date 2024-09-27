530648baba1055e41e4c6893680d3185839352e6
Newvber <weibo2@ualberta.ca>
2024-09-26 18:46:13 -0600

    conflict

--- app/src/main/java/Shape.java
+++ app/src/main/java/Shape.java
@@ -1,9 +1,11 @@
 class Shape {
     private int x;
     private int y;
-
+    private String color = "red";
     public Shape(int xval, int yval) {
         this.x = xval;
         this.y = yval;
     }
+
+
 }

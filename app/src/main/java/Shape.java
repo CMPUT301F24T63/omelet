f1cfc4f5b468e7bc55d6e0e358aaa3e5e3b458fd
“Dingjingmu <stevenydjm@gmail.com>
2024-09-26 18:45:02 -0600

    d

--- app/src/main/java/Shape.java
+++ app/src/main/java/Shape.java
@@ -1,9 +1,11 @@
 class Shape {
     private int x;
     private int y;
+    private String color;
 
-    public Shape(int xval, int yval) {
+    public Shape(int xval, int yval, String color) {
         this.x = xval;
         this.y = yval;
+        this.color = color;
     }
 }
